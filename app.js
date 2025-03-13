e77d8bb0742c7f395aabb65b7d070eddb824702f
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-13 21:37:59 +0100

    Refactor posts router to import posts data from a separate module and improve route handling

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const postsRouter = require('./routers/posts');
+const postsRouter = require('./routers/posts'); 
 const app = express();
 const port = 3001;
 
 app.listen(port, () => {
-    console.log(`Server is working on http://localhost${port}`);
+    console.log(`Server is working on http://localhost:${port}`);
 
 });
 
